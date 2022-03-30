c0a3cb10ccb6d8a1abfda5cd823137f80108be1a
christy <bikoyichristy@hotmail.fr>
2022-03-30 11:53:02 +0200

    advance

--- header.php
+++ header.php
@@ -5,14 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biliothèquev2</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="forme.css">
     <script type="text/javascript"  src="ouvrirfenetre.js"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha512-OvBgP9A2JBgiRad/mM36mkzXSXaJE9BEIENnVEmeZdITvwT09xnxLtT4twkCa8m/loMbPHsvPl0T8lRGVBwjlQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>    <title>Bibliothèque</title>
     <link href="https://fonts.googleapis.com/css2?family=Kameron&display=swap" rel="stylesheet">
-    <?php 
+</head>
+<body>
+<div class="entete">
+<?php 
     session_start();
     $isLoggedIn = false;
     if(isset($_SESSION["mail"])){
@@ -21,13 +23,15 @@
         $isLoggedIn = true;
     } 
 ?>
-</head>
-<body>
-<ul>
+<h1>Bibliothèque Manga en Ligne</h1>
+<ul class="menu">
     <li><a href="index.php">Home</a></li>
     <?php if($isLoggedIn){ ?><li><a href="catalogue.php">Catalogue</a></li><?php } ?>
     <li><a href="senrengistrer.php">S'inscrire</a></li>
     <li><a href="seconnecter.php">Se connecter</a></li>
     <?php if($isLoggedIn){ ?><li><a href="panier.php">Panier</a></li><?php } ?>
 </ul>
+<div class="buttondiv">
 <?php if($isLoggedIn){ ?><button><a href="deconnexion.php">Se Deconnecter</a></button><?php } ?>
+</div>
+</div>
--- index.php
+++ index.php
@@ -2,46 +2,41 @@
    <?php
    include "header.php";
    ?>
-  <div>
-    <img alt="banniere mangas" class="imglogo" src="https://p4.wallpaperbetter.com/wallpaper/518/375/797/manga-anime-mix-up-hd-wallpaper-preview.jpg"/>
-  </div>
+
   <!-- Page Titre + Paragraphe -->
-  <h1>Bibliothèque Manga en Ligne</h1>
-  <p>
-      Lorem ipsum"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum."
+  <div class="descriptiondiv">
+  <p class="description">
+     Bienvenu sur la première Bibliothèque en ligne basé sur les nouveautés mangas, imprénié vous de la culture nippone (japonaise). En découvrant des oeuvres phares de la culture japonaises.
+     Inscrivez vous afin d'accèder au catalogue en ligne et pouvoir emprunter les mangas de vos choix. Vous trouvez les incontournables comme dragonball Z, Naruto, One piece.
+     Les oeuvres du moment comme attaque des titans, tokyo revengers et bien d'autres.
   </p>
+</div>
   <h2>Nouveautés</h2>
-  <div id="carouselExampleDark" class="carousel carousel-dark slide w-20" data-bs-ride="carousel">
+  <div id="carouselExampleDark" class="carousel carousel-dark slide w-5" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="1000">
-          <img src="https://cdn.verasia.fr/19334-big_default_2x/boruto-vol-14-naruto-next-generations.jpg" class="d-block w-10" alt="...">
+        <div class="carousel-item active"  style="text-align:center;" data-bs-interval="1000">
+          <img src="https://cdn.verasia.fr/19334-big_default_2x/boruto-vol-14-naruto-next-generations.jpg" class="d-block" style="height:500px; width:400px; text-align:center;" alt="...">
           <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
+            <h5>Boruto </h5>
             <p>Some representative placeholder content for the first slide.</p>
           </div>
         </div>
         <div class="carousel-item" data-bs-interval="2000">
-          <img src="https://www.manga-news.com/public/images/series/attaque-des-titans-birth-of-livai-1-pika.jpg" class="d-block w-10" alt="...">
+          <img src="https://www.manga-news.com/public/images/series/attaque-des-titans-birth-of-livai-1-pika.jpg" class="d-block" style="height:500px; width:400px;" alt="...">
           <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
+            <h5>Attaque des titants</h5>
             <p>Some representative placeholder content for the second slide.</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="https://bleachmx.fr/wp-content/uploads/Seven-Deadly-Sins-tome-40.jpg" class="d-block w-10" alt="...">
+          <img src="https://bleachmx.fr/wp-content/uploads/Seven-Deadly-Sins-tome-40.jpg" class="d-block" style="height:500px; width:400px;" alt="...">
           <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
+            <h5>Seven Deadly Sins</h5>
             <p>Some representative placeholder content for the third slide.</p>
           </div>
         </div>
@@ -56,7 +51,9 @@
       </button>
     </div>
     </div>
-  <button type="button" class="btn btn-outline-primary">Accéder au Catalogue</button>
+    <div class="buttoninscription">
+      <button type="button" class="btn btn-outline-primary">Accéder au Catalogue</button>
+    </div>
   <!-- Footer  -->
   <ul>
       <li>Home</li>
